4a4963464cead4124fcb02bc7dcedb08d68c477c
hyuneijini <bigglasscup@naver.com>
2026-09-12 08:22:21 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+const menuButton = document.querySelector('.menu-toggle');
+const mobileNav = document.querySelector('.mobile-nav');
+
+menuButton.addEventListener('click', () => {
+  const isOpen = mobileNav.classList.toggle('is-open');
+  menuButton.setAttribute('aria-expanded', String(isOpen));
+});
+
+mobileNav.querySelectorAll('a').forEach((link) => {
+  link.addEventListener('click', () => {
+    mobileNav.classList.remove('is-open');
+    menuButton.setAttribute('aria-expanded', 'false');
+  });
+});
